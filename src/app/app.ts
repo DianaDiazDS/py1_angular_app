c5108cddce802ebe73b3a05c4233985ebdc6c975
DianaDiazDS <93956532+DianaDiazDS@users.noreply.github.com>
2025-07-05 16:41:58 -0500

    primer commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -10,3 +10,19 @@ import { RouterOutlet } from '@angular/router';
 export class App {
   protected title = 'py1_angular_app';
 }
+
+/**
+ * Componente principal de la aplicación Angular.
+ *
+ * @component
+ * @selector app-root - Selector utilizado para insertar este componente en el HTML.
+ * @imports RouterOutlet - Importa el RouterOutlet, que permite la navegación entre rutas hijas dentro de la aplicación.
+ * @templateUrl ./app.html - Ruta al archivo de plantilla HTML asociado a este componente.
+ * @styleUrl ./app.css - Ruta al archivo de estilos CSS asociado a este componente.
+ */
+// export class App {
+  /**
+   * Título de la aplicación, utilizado como propiedad protegida.
+   */
+//   protected title = 'py1_angular_app';
+// }
