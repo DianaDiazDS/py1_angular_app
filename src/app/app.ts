13e3853c83d5e1c1580ec6f32ab31527821da540
DianaDiazDS <93956532+DianaDiazDS@users.noreply.github.com>
2025-07-06 16:32:10 -0500

    visualizacion de estructuras

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,34 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { CommonModule } from '@angular/common';
+//este import es necesario para poder usar las directivas ngIf, ngFor, etc. en el HTML del componente.
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
+
 })
 export class App {
-  protected title = 'py1_angular_app';
-}
+  protected title = 'Angular APP';
+  title2 = 'Angular APP';
 
+  componente2 = 'un string';
+  componente3 = 34;
+  componentes4 = [1, 2, 3, 4, 5];
+  componentes5 = ['ana', 'pepe', 'luis', 'maria', 'juan'];
+  componente6 = {key : 'value', key2: 'value2', key3: 'value3'};
+  componentes7 = [
+  { id: 111, valor: 'valor1', persona: 'persona1' },
+  { id: 222, valor: 'valor2', persona: 'persona2' },
+  { id: 333, valor: 'valor3', persona: 'persona3' }
+  ]
+  componentes8 = []
+
+  visible: boolean = false;
+}
 /**
  * Componente principal de la aplicación Angular.
  *
